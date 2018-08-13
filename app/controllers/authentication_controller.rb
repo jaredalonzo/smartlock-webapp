8cff346ee498730f956e75f68bf451958335d443
Jared <jared.alonzo@icloud.com>
2018-08-13 11:58:17 -0700

    Current problems: need to access correct session token

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,12 +1,40 @@
+require 'uri'
+require 'json'
+require 'net/http'
 class AuthenticationController < ApplicationController
   def create
-    #user = request.env['omniauth.auth']
-    #response = client.auth_code.get_token
+    user = request.env['omniauth.auth'].to_yaml
+    
+#    render text: user.to_yaml
+  
+    puts '
+    123
+    '
 
-    puts params[:code].to_s
+    Rails.logger.info(request.env)
 
-    session[:access_token] = params[:code]
+    puts '
+    456
+    '
 
-    puts session[:access_token].to_s
+    endpoints_uri = 'https://graph.api.smartthings.com/api/smartapps/endpoints'
+
+    puts ' Token is: ' + session[:access_token]
+
+#    url = URI.parse(endpoints_uri)
+#    req = Net::HTTP::Get.new(url.request_uri)
+#
+#    req['Authorization'] = 'Bearer ' + token
+#
+#    http = Net::HTTP.new(url.host, url.port)
+#    http.use_ssl = (url.scheme == 'https')
+#
+#    response = http.request(req)
+#    json = JSON.parse(response.body)
+#
+#    puts json
+
+    #response = RestClient::Request.execute(method: :get, url: endpoints_uri, headers: {api_key: token})
+    
   end
 end
