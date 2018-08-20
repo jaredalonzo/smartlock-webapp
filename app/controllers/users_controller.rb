02e36219503ec262cca5be0bbd8e7596020e21fc
Jared <jared.alonzo@icloud.com>
2018-08-20 11:32:03 -0700

    Show button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,54 @@
+require 'net/http'
+require 'json'
+
+ENDPOINTS_URI = 'https://graph.api.smartthings.com/api/smartapps/endpoints'
+
 class UsersController < ApplicationController
   def index
     @users = User.all
   end
 
   def show
+    token = session[:access_token]
     @user = User.find(params[:id])
+    url = URI.parse(ENDPOINTS_URI)
+    req = Net::HTTP::Get.new(url.request_uri)
+
+    req['Authorization'] = 'Bearer ' + token
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = (url.scheme == 'https')
+
+    response = http.request(req)
+    json = JSON.parse(response.body)
+
+    puts json
+
+    uri = json[0]['uri']
+    
+    puts 'Probing door lock'
+
+    doorURL = uri + '/keys'
+    getdoorURL = URI.parse(doorURL)
+    getdoorReq = URI.parse(doorURL)
+    getdoorReq = Net::HTTP::Get.new(getdoorURL.request_uri)
+    getdoorReq['Authorization'] = 'Bearer ' + token
+    getdoorHTTP = Net::HTTP.new(getdoorURL.host, getdoorURL.port)
+    getdoorHTTP.use_ssl = true
+
+    doorStatus = getdoorHTTP.request(getdoorReq)
+
+    puts doorStatus.body
+    
   end
   
   def new
+    token = session[:access_token]
     @user = User.new
   end
 
   def edit
+    token = session[:access_token]
     @user = User.find(params[:id])
   end
 
